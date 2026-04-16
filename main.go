30c8fcdc32daf7956e280520699885e5d1f7ff4b
Andrei <itunereg@gmail.com>
2026-04-16 10:36:02 +0300

    input and empty calc

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	const UsdToEur = 0.85
 	const UsdToRub = 75.38
 	const EurToRub = UsdToRub / UsdToEur
+
+	input := userInput()
+}
+
+func userInput() string {
+	var input string
+
+	fmt.Scan(&input)
+	return input
+}
+
+func calc(count float64, curr1 string, curr2 string) (res float64) {
+
+	return
 }
