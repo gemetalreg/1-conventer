7464d7c1befa6fd75a777955ad700126a2c25c12
Andrei <itunereg@gmail.com>
2026-04-18 18:36:54 +0300

    calc

--- main.go
+++ main.go
@@ -3,21 +3,82 @@ package main
 import "fmt"
 
 func main() {
-	const UsdToEur = 0.85
-	const UsdToRub = 75.38
-	const EurToRub = UsdToRub / UsdToEur
+	curr1 := userInput()
+	num := userInputNum()
+	curr2 := userInputTarget(curr1)
 
-	input := userInput()
+	res := calc(num, curr1, curr2)
+	fmt.Println(res)
+
 }
 
 func userInput() string {
 	var input string
+	for {
+		fmt.Print("Введите исходную валюту USD, EUR, RUB")
+		fmt.Scan(&input)
+		if input == "USD" || input == "EUR" || input == "RUB" {
+			break
+		}
+	}
+	return input
+}
 
-	fmt.Scan(&input)
+func userInputNum() float64 {
+	var input float64
+	for {
+		fmt.Print("Введите число")
+		_, err := fmt.Scan(&input)
+
+		if err != nil {
+			fmt.Println("Введите число")
+		} else {
+			break
+		}
+	}
+	return input
+}
+
+func userInputTarget(curr1 string) string {
+	var input string
+	for {
+		fmt.Print("Введите целевую валюту USD, EUR, RUB")
+		fmt.Scan(&input)
+		if input == curr1 {
+			fmt.Printf("Ваша целевая валюта %s не должна совпадать с целевой валютой %s\n", curr1, input)
+			continue
+		}
+
+		if input == "USD" || input == "EUR" || input == "RUB" {
+			break
+		}
+	}
 	return input
 }
 
 func calc(count float64, curr1 string, curr2 string) (res float64) {
-
-	return
+	switch curr1 {
+	case "USD":
+		switch curr2 {
+		case "EUR":
+			return 0.85 * count
+		case "RUB":
+			return 76.23 * count
+		}
+	case "EUR":
+		switch curr2 {
+		case "USD":
+			return 1.18 * count
+		case "RUB":
+			return 89.65 * count
+		}
+	case "RUB":
+		switch curr2 {
+		case "USD":
+			return 0.013 * count
+		case "EUR":
+			return 0.011 * count
+		}
+	}
+	return 0
 }
